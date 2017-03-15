c55003fa9ef829478b6f9a7cf051bebafe9c3818
John  Ito lee <johnitolee@Ito.local>
2017-03-14 18:28:47 -0700

    working login signin logout

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './app';
 import { Provider } from 'react-redux';
+import SessionFormContainer from './session_form/session_form_container';
 
 
 const Root = ({ store }) => (
   <Provider store={ store }>
     <Router history={ browserHistory }>
       <Route path="/" component={ App }/>
+      <Route path="/login" component={ SessionFormContainer }/>
+      <Route path="/signup" component={ SessionFormContainer }/>
     </Router>
   </Provider>
 );
