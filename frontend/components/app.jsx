c858e2086e0bbea532d913712a90665af31f612e
John  Ito lee <johnitolee@Ito.local>
2017-03-14 19:42:13 -0700

    includes navbar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import GreetingContainer from './greeting/greeting_container';
+// import GreetingContainer from './greeting/greeting_container';
+import NavbarContainer from './navbar/navbar_container';
 
-const App = ({children}) => (
-  <div>
-    <h1>EventureBrite</h1>
-    <GreetingContainer />
-    {children}
-  </div>
-);
+const App = (store) => {
+  return (
+    <div>
+      <h1>EventureBrite</h1>
+      <NavbarContainer />
+      {store.children}
+    </div>
+  );
+};
 
 export default App;
+
+// <GreetingContainer />
---
+++ frontend/components/navbar/navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Greeting from '../greeting/greeting'; // change here.
+
+class Navbar extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+    return(
+      <header>
+        <h2>Logo Here</h2>
+        <Greeting currentUser={this.props.currentUser}
+          logout={this.props.logout} />
+      </header>
+    );
+  }
+}
+
+export default Navbar;
