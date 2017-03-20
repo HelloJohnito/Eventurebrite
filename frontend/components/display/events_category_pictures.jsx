a10f0cb2dc7dd62e0e47dbb2621d20ab824296a4
John  Ito lee <johnitolee@Ito.local>
2017-03-19 21:31:35 -0700

    finished category pictures on homepage

--- frontend/components/display/events_category_pictures.jsx
+++ frontend/components/display/events_category_pictures.jsx
@@ -13,6 +13,10 @@ const EventsCategoryPictures = () => (
         <Link to="/browse/sports">
           <div className="category-pictures-sports">
             <img className="sports-picture" src="http://www.pinnacleadventure.co.uk/perch/resources/cycletoursedinburgh.jpg"/>
+            <div className="category-pictures-sports-title">Sports</div>
+            <span className="category-pictures-sports-description">
+              Snowboard, Bike, Surf
+            </span>
           </div>
         </Link>
       </div>
@@ -21,6 +25,10 @@ const EventsCategoryPictures = () => (
         <Link to="/browse/networking">
           <div className="category-pictures-networking">
             <img className="networking-picture" src="http://www.bloggedtopics.com/wp-content/uploads/2016/12/customers-business-320x220.jpg"/>
+            <div className="category-pictures-networking-title">Networking</div>
+            <span className="category-pictures-networking-description">
+              Business Mixers, Hobby Meetups, Panel Discussions
+            </span>
           </div>
         </Link>
       </div>
@@ -29,6 +37,10 @@ const EventsCategoryPictures = () => (
         <Link to="/browse/classes">
           <div className="category-pictures-classes">
             <img className="classes-picture" src="https://i1.wp.com/sosphysiotherapy.ca/wp-content/uploads/2013/03/yoga.jpg?fit=320%2C220" />
+            <div className="category-pictures-classes-title">Classes</div>
+            <span className="category-pictures-classes-description">
+              Breakdance, Yoga, Fitness
+            </span>
           </div>
         </Link>
       </div>
@@ -37,6 +49,10 @@ const EventsCategoryPictures = () => (
         <Link to="/browse/arts">
           <div className="category-pictures-arts">
             <img className="arts-picture" src="http://www.advaitastoian.com/wp-content/themes/MainEvents/timthumb.php?src=http://www.advaitastoian.com/wp-content/uploads/2013/12/yogacampcostinesti2013.jpg&h=220&w=350&zc=1&q=100"/>
+            <div className="category-pictures-arts-title">Arts</div>
+            <span className="category-pictures-arts-description">
+              Art Exhibitions, Comedy Nights, Plays
+            </span>
           </div>
         </Link>
       </div>
@@ -45,6 +61,10 @@ const EventsCategoryPictures = () => (
         <Link to="/browse/parties">
           <div className="category-pictures-parties">
             <img className="parties-picture" src="http://www.nappertandysirishpub.com/napperscatering/images/Birthday-Parties.jpg" />
+            <div className="category-pictures-parties-title">Parties</div>
+            <span className="category-pictures-parties-description">
+              Casual Nights Out, Happy Hours, Themed Parties
+            </span>
           </div>
         </Link>
       </div>
@@ -53,14 +73,22 @@ const EventsCategoryPictures = () => (
         <Link to="/browse/music">
           <div className="category-pictures-music">
             <img className="music-picture" src="http://cdn.ebaumsworld.com/thumbs/2012/11/02/083113/82887930/concerz.jpg" />
+            <div className="category-pictures-music-title">Music</div>
+            <span className="category-pictures-music-description">
+              Music Festivals, Dance Parties, Live Concerts
+            </span>
           </div>
         </Link>
       </div>
 
       <div className="category-pictures-outer-large">
         <Link to="/browse/food">
-          <div className="category-pictures-foods">
+          <div className="category-pictures-food">
             <img className="food-picture" src="https://www.designholidays.co.uk/wp-content/uploads/2014/10/vidamar-madeira-15.jpg"/>
+            <div className="category-pictures-food-title">Food</div>
+            <span className="category-pictures-food-description">
+              Tastings, Food Challenges, Dinner Parties
+            </span>
           </div>
         </Link>
       </div>
