9add5192a6aff8a4d89f60b7a5b77c0513c8ebe1
John <johnitolee@gmail.com>
2017-04-30 13:17:43 -0700

    refactor items pages

--- frontend/components/events/event_browse_item.jsx
+++ frontend/components/events/event_browse_item.jsx
@@ -3,35 +3,36 @@ import { Link } from 'react-router';
 import BookmarkContainer from '../bookmarks/bookmark_container';
 
 
-const EventBrowseItem = (props) => (
-  <li className="browse-event">
-    <div className="browse-event-o">
+// const EventBrowseItem = (props) => (
+function EventBrowseItem(props){
+  return (
+    <li className="browse-event">
+      <div className="browse-event-o">
 
-      <div className="browse-event-inner">
-        <Link to={`/events/${props.eventId}`}>
-        <div className="browse-event-image">
-            <img className="browse-event-image-url" src={props.image} />
-        </div>
-        </Link>
-
-        <div className="browse-event-info">
-          <div className="browse-event-info-top">
-            <div className="browse-event-date">{props.date}</div>
-            <div className="browse-event-title">{props.title}</div>
-            <div className="browse-event-location">{props.location}</div>
+        <div className="browse-event-inner">
+          <Link to={`/events/${props.eventId}`}>
+          <div className="browse-event-image">
+              <img className="browse-event-image-url" src={props.image} />
           </div>
-          <div className="browse-event-info-bottom">
-            <div className="browse-event-category">{props.categories}</div>
-            <div className="browse-event-bookmark-icon">
-              <BookmarkContainer eventId={props.eventId} />
+          </Link>
+
+          <div className="browse-event-info">
+            <div className="browse-event-info-top">
+              <div className="browse-event-date">{props.date}</div>
+              <div className="browse-event-title">{props.title}</div>
+              <div className="browse-event-location">{props.location}</div>
+            </div>
+            <div className="browse-event-info-bottom">
+              <div className="browse-event-category">{props.categories}</div>
+              <div className="browse-event-bookmark-icon">
+                <BookmarkContainer eventId={props.eventId} />
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-
-  </div>
-  </li>
-);
+    </li>
+  );
+}
 
 export default EventBrowseItem;
--- frontend/components/events/events_index_item.jsx
+++ frontend/components/events/events_index_item.jsx
@@ -2,27 +2,30 @@ import React from 'react';
 import { Link } from 'react-router';
 import BookmarkContainer from '../bookmarks/bookmark_container';
 
-const EventsIndexItem = (props) => (
-  <li className="index-event">
-    <Link to={`/events/${props.eventId}`}>
-      <div className="index-event-image">
-          <img className="index-event-image-url" src={props.image} />
-      </div>
-      <div className="index-event-info">
-        <div className="index-event-info-top">
-          <div className="index-event-date">{props.date}</div>
-          <div className="index-event-title">{props.title}</div>
-          <div className="index-event-location">{props.location}</div>
+// const EventsIndexItem = (props) => (
+function EventsIndexItem(props){
+  return(
+    <li className="index-event">
+      <Link to={`/events/${props.eventId}`}>
+        <div className="index-event-image">
+            <img className="index-event-image-url" src={props.image} />
         </div>
-        <div className="index-event-info-bottom">
-          <div className="index-event-category">{props.categories}</div>
+        <div className="index-event-info">
+          <div className="index-event-info-top">
+            <div className="index-event-date">{props.date}</div>
+            <div className="index-event-title">{props.title}</div>
+            <div className="index-event-location">{props.location}</div>
+          </div>
+          <div className="index-event-info-bottom">
+            <div className="index-event-category">{props.categories}</div>
+          </div>
         </div>
+      </Link>
+      <div className="index-event-bookmark-icon">
+        <BookmarkContainer eventId={props.eventId}/>
       </div>
-    </Link>
-    <div className="index-event-bookmark-icon">
-      <BookmarkContainer eventId={props.eventId}/>
-    </div>
-  </li>
-);
+    </li>
+  );
+}
 
 export default EventsIndexItem;
