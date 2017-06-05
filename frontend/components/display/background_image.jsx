ce7ee5232150ecf293ad588ff203878623c7d517
John <johnitolee@gmail.com>
2017-06-05 13:07:37 -0700

    update pictures for seeding

--- frontend/components/display/background_image.jsx
+++ frontend/components/display/background_image.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const BackgroundImage = () => (
-  <img className="detail-background-image" src="https://res.cloudinary.com/deh9l9lyq/image/upload/v1490027634/light_wtlzlq.jpg" />
+  <img className="detail-background-image" src="https://res.cloudinary.com/deh9l9lyq/image/upload/q_auto/v1490027634/light_wtlzlq.jpg" />
 );
 
 export default BackgroundImage;
